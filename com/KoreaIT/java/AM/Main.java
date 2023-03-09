d10bc1ac705c84e9940ce71b2dbea0e7e63c0c09
guenhyuck <guenhyuck94@gmail.com>
2023-03-09 14:55:02 +0900

    게시물 상세보기 구현중

--- com/KoreaIT/java/AM/Main.java
+++ com/KoreaIT/java/AM/Main.java
@@ -1,5 +1,4 @@
 package com.KoreaIT.java.AM;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,66 +7,46 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-		
 		int lastArticleId = 0;
 		List<Article> articles = new ArrayList<>();
-		
-
 		while (true) {
-			System.out.printf("명령어 ) ");
+
+			System.out.print("명령어 > ");
+		
 			String command = sc.nextLine().trim();
 
 			if (command.length() == 0) {
-				System.out.println("명령어를 입력해 주세요.");
-				continue;
-			}
-			if (command.equals("system exit")) {
-				break;
-			}
-			if (command.equals("article write")) {
-				int id = lastArticleId + 1;
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
+				System.out.println("명령어를 입력해주세요");
 
-				
-				Article article = new Article(id, title, body);
-				articles.add(article);
-
-				
-				System.out.printf("%d번 게시글이 생성되었습니다.\n", id);
+				System.out.printf("%d번글이 생성되었습니다\n", id);
 				lastArticleId++;
 
-			} else if (command.equals("article list")) {
-				if (articles.size() == 0) {
-					System.out.println("등록된 게시글이 없습니다.");
-					continue;
-				}
+			} else if (command.startsWith("article detail ")) {
 
-				System.out.println("  번호   /   제목   /   내용 ");
+				String[] cmdDiv = command.split(" "); // article detail 1
 
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+//				System.out.println(cmdDiv[0]);
+//				System.out.println(cmdDiv[1]);
+//				System.out.println(cmdDiv[2]);
 
-					System.out.printf("   %d   /   %s    /     %s \n", article.id, article.title, article.body);
-				}
+				int id = Integer.parseInt(cmdDiv[2]);
+
+				// article detail 1 => "1" => 1
+
+				System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 
 			} else {
-				System.out.println("존재하지 않는 명령어입니다.");
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
-		System.out.println("==프로그램 종료==");
-
+		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
 }
-
 class Article {
 	int id;
 	String title;
 	String body;
-
 	Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
