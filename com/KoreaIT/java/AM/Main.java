614fbf724cb52838c7da864185a8597d263c7282
guenhyuck <guenhyuck94@gmail.com>
2023-03-09 13:52:20 +0900

    게시물 목록

--- com/KoreaIT/java/AM/Main.java
+++ com/KoreaIT/java/AM/Main.java
@@ -1,6 +1,5 @@
 package com.KoreaIT.java.AM;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,58 +8,52 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-
-		List<Article> articles = new ArrayList<>(); 
-
+		
 		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<>();
+		
 
-		while(true) {
+		while (true) {
 			System.out.printf("명령어 ) ");
-			String command = sc.nextLine().trim(); 
+			String command = sc.nextLine().trim();
 
-			if(command.length()==0) {     
+			if (command.length() == 0) {
 				System.out.println("명령어를 입력해 주세요.");
 				continue;
 			}
-			if(command.equals("system exit"))
-			{
-				break;     
+			if (command.equals("system exit")) {
+				break;
 			}
-			if(command.equals("article write")) {
-				int id = lastArticleId+1;      
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
-				String title = sc.nextLine();      
+				String title = sc.nextLine();
 				System.out.printf("내용 : ");
-				String body = sc.nextLine(); 
+				String body = sc.nextLine();
+
 				
-				
-				Article article = new Article(id, title, body);    
-				articles.add(article);      
-				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
 				
 				System.out.printf("%d번 게시글이 생성되었습니다.\n", id);
-				lastArticleId++;      
-				
-			}
-			else if(command.equals("article list")) {
-				 if(articles.size()==0) {      
+				lastArticleId++;
+
+			} else if (command.equals("article list")) {
+				if (articles.size() == 0) {
 					System.out.println("등록된 게시글이 없습니다.");
 					continue;
 				}
-				
-				
+
 				System.out.println("  번호   /   제목   /   내용 ");
-				
-				
-				
-				for(int i = articles.size()-1; i >= 0 ; i--) {      
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					
+
 					System.out.printf("   %d   /   %s    /     %s \n", article.id, article.title, article.body);
 				}
 
-			}
-			else {
+			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
 		}
@@ -69,6 +62,7 @@ public class Main {
 		sc.close();
 	}
 }
+
 class Article {
 	int id;
 	String title;
