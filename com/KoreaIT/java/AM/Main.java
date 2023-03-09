9a12368431aa8e2be818e4eb50cdebf234383969
guenhyuck <guenhyuck94@gmail.com>
2023-03-09 15:05:19 +0900

    게시물 상세보기 구현중(오류수정)

--- com/KoreaIT/java/AM/Main.java
+++ com/KoreaIT/java/AM/Main.java
@@ -12,12 +12,34 @@ public class Main {
 		while (true) {
 
 			System.out.print("명령어 > ");
-		
+			
 			String command = sc.nextLine().trim();
 
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
-
+				continue;
+			}
+			if (command.equals("exit")) {
+				break;
+			}
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+				} else {
+					System.out.println(" 번호  //  제목  ");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("  %d   //   %s  \n", article.id, article.title);
+					}
+				}
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				Article article = new Article(id, title, body);
+				articles.add(article);
 				System.out.printf("%d번글이 생성되었습니다\n", id);
 				lastArticleId++;
 
